c1e84b3580307d6d0bce92ad0268f5ee59381bb7
Vamshi Basupalli <vamshi@cs.wisc.edu>
2017-04-25 11:45:29 -0500

    Added support multiple assessment
    Added support delete assessment record
    
    Added simplifing assessment status

--- src/main/java/org/continuousassurance/swamp/api/PlatformVersion.java
+++ src/main/java/org/continuousassurance/swamp/api/PlatformVersion.java
@@ -7,13 +7,44 @@ import org.continuousassurance.swamp.session.Session;
  * on 3/4/15 at  12:39 PM
  */
 public class PlatformVersion extends SwampThing {
-    public PlatformVersion(Session session) {
+    public static final String NAME_KEY ="full_name";
+    public static final String PLATFORM_UUID_KEY="platform_uuid";  
+    public static final String PLATFORM_VERSION_UUID_KEY ="platform_version_uuid";
+    public static final String VERSION_STRING ="version_string";
+
+    protected Platform platform;
+	
+    public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+
+	public PlatformVersion(Session session) {
         super(session);
     }
 
+	public String getFullName() {
+		return this.getConversionMap().getString(NAME_KEY);
+	}
+
+	public String getVersionString() {
+		return this.getConversionMap().getString(VERSION_STRING);
+	}
+
+	public String getPlatformVersionUuid() {
+		return this.getConversionMap().getString(PLATFORM_VERSION_UUID_KEY);
+	}
+
+	public String getName() {
+		return getFullName();
+	}
+	
     @Override
     public String getIDKey() {
-        return null;
+        return PLATFORM_VERSION_UUID_KEY;
     }
 
     @Override

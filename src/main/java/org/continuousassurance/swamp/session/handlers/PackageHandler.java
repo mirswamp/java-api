7fea314cae2f7f829d3397853db3fd87cdf7a805
Vamshi Basupalli <vamshi@cs.wisc.edu>
2017-05-03 10:48:59 -0500

    Changed package types return value to a Map from a List

--- src/main/java/org/continuousassurance/swamp/session/handlers/PackageHandler.java
+++ src/main/java/org/continuousassurance/swamp/session/handlers/PackageHandler.java
@@ -8,7 +8,9 @@ import org.continuousassurance.swamp.session.util.ConversionMapImpl;
 import net.sf.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>Created by Jeff Gaynor<br>
@@ -130,7 +132,7 @@ public class PackageHandler<T extends PackageThing> extends AbstractHandler<T> {
         return createURL("packages");
     }
 
-
+    /*
     public List<String> getTypes() {
         String url = createURL("packages/types");
         MyResponse mr = getClient().rawGet(url, null);
@@ -140,6 +142,18 @@ public class PackageHandler<T extends PackageThing> extends AbstractHandler<T> {
         	pkg_types.add(mr.jsonArray.getJSONObject(i).getString("name"));
         }
         return pkg_types;
+    }*/
+
+    public Map<String, Integer> getTypes() {
+    	String url = createURL("packages/types");
+    	MyResponse mr = getClient().rawGet(url, null);
+
+    	Map<String, Integer> pkg_types = new HashMap<String, Integer>();
+    	for (int i = 0; i < mr.jsonArray.size(); ++i){
+    		pkg_types.put(mr.jsonArray.getJSONObject(i).getString("name"),
+    				Integer.valueOf(mr.jsonArray.getJSONObject(i).getInt("package_type_id")));
+    	}
+    	return pkg_types;
     }
 
     /*
